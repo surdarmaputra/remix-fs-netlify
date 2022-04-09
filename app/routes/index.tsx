c76f1ab723e02e443890d8baf173ade22ba65c8f
Surya Darma Putra <surya.igede@gmail.com>
2022-04-09 23:05:33 +0800

    enable debug

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,19 +1,21 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import type { Post } from "~/services/post.server";
-import { getPosts } from "~/services/post.server";
+import { getPosts, getDir } from "~/services/post.server";
 
 interface LoaderData {
   posts: Post[];
+  dir: Record<string, string>;
 }
 
 export const loader: LoaderFunction = async () => {
   const posts = await getPosts();
-  return {posts};
+  const dir = await getDir();
+  return {posts, dir};
 }
 
 export default function Index() {
-  const { posts} = useLoaderData<LoaderData>()
+  const { posts, dir } = useLoaderData<LoaderData>()
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Posts</h1>
@@ -21,6 +23,13 @@ export default function Index() {
         <div key={title}>
           <div>{title}</div>
           <div>{content}</div>
+        </div>
+      ))}
+      <hr />
+      {Object.entries(dir).map(([key, value]) => (
+        <div key={key}>
+          <div>{key} : </div>
+          <div>{value}</div>
         </div>
       ))}
     </div>
